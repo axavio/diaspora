65f10b0cad42a4f949dedda7b788a1f2a1cfc119
Dave Yingling <david.yingling@gmail.com>
2012-07-22 07:00:07 -0700

    add check for user_signed_in? to nsfw filter

--- app/helpers/stream_element_helper.rb
+++ app/helpers/stream_element_helper.rb
@@ -18,7 +18,7 @@ module StreamElementHelper
   end
 
   def nsfw_shield(post)
-    if post.respond_to?(:nsfw?) && post.nsfw? && !current_user.nsfw_shield_disabled
+    if post.respond_to?(:nsfw?) && post.nsfw? && ( !user_signed_in? || !current_user.nsfw_shield_disabled )
       content_tag(:div, :class => 'shield') do
         I18n.translate('shared.stream_element.nsfw', :link => link_to(I18n.translate('shared.stream_element.show'), '#', :title => t('._nsfw_tip'))).html_safe
       end
